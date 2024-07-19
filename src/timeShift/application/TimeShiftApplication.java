9fba6dd4478ebb1af9f1628c7ccd88424abd997a
KlnSdr <monsieurk1209@gmail.com>
2024-07-19 21:49:57 +0200

    initial commit

---
+++ src/timeShift/application/TimeShiftApplication.java
@@ -0,0 +1,9 @@
+package timeShift.application;
+
+import hades.Hades;
+
+public class TimeShiftApplication extends Hades {
+    public static void main(String[] args) {
+        new TimeShiftApplication().startApplication(TimeShiftApplication.class);
+    }
+}
