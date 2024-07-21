d45591a4e5cfca6cd209c80159718ee99828f4fa
KlnSdr <monsieurk1209@gmail.com>
2024-07-22 00:32:41 +0200

    add left/right button to calendar

--- src/timeShift/application/public/mainScreen/components/calendarPanel/calendarPanel.ts
+++ src/timeShift/application/public/mainScreen/components/calendarPanel/calendarPanel.ts
@@ -7,7 +7,21 @@ class calendarPanel implements Component {
     return {
       tag: "div",
       classes: ["calendarPanel"],
-      children: [new calendar().instructions()],
+      children: [
+        {
+          tag: "div",
+          classes: ["calendarHeader"],
+          children: [
+            new button("<", () => {}, ["squareButtonSmol"]).instructions(),
+            {
+              tag: "label",
+              text: "Juli 2024",
+            },
+            new button(">", () => {}, ["squareButtonSmol"]).instructions(),
+          ],
+        },
+        new calendar().instructions(),
+      ],
     };
   }
 
