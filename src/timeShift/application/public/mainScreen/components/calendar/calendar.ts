0b36298bf2b1a070cdc48b09ac363eababb069ef
KlnSdr <monsieurk1209@gmail.com>
2024-07-22 01:04:43 +0200

    add way to display correct date-weekday combination

--- src/timeShift/application/public/mainScreen/components/calendar/calendar.ts
+++ src/timeShift/application/public/mainScreen/components/calendar/calendar.ts
@@ -8,6 +8,18 @@ class calendar implements Component {
       tag: "div",
       classes: ["calendar"],
       children: [
+        ...["M", "D", "M", "D", "F", "S", "S"].map((day: string) =>
+          new button(day, () => {}, [
+            "squareButton",
+            "disabledButton",
+          ]).instructions(),
+        ),
+        ...[1, 2, 3].map((i: number) =>
+          new button("", () => {}, [
+            "squareButton",
+            "disabledButton",
+          ]).instructions(),
+        ),
         ...Array.from({ length: 31 }, (_, i) => i + 1).map((day: number) =>
           new button(day.toString(), () => {}, ["squareButton"]).instructions(),
         ),
