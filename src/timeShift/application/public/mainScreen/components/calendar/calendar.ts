3335c7377a3d0f14939fd2929e7a8b6876835f54
KlnSdr <monsieurk1209@gmail.com>
2024-07-23 22:01:39 +0200

    calendar is now a true calendar

--- src/timeShift/application/public/mainScreen/components/calendar/calendar.ts
+++ src/timeShift/application/public/mainScreen/components/calendar/calendar.ts
@@ -1,4 +1,12 @@
 class calendar implements Component {
+  private readonly month: number;
+  private readonly year: number;
+
+  public constructor(month: number, year: number) {
+    this.month = month;
+    this.year = year;
+  }
+
   public render(parent: edomElement) {
     edom.fromTemplate([this.instructions()], parent);
   }
@@ -14,17 +22,41 @@ class calendar implements Component {
             "disabledButton",
           ]).instructions(),
         ),
-        ...[1, 2, 3].map((i: number) =>
+        ...Array.from(
+          { length: this.jsWeekdayToOffset(this.getWeekdayOfFirstDay()) },
+          (_, i) => i + 1,
+        ).map((i: number) =>
           new button("", () => {}, [
             "squareButton",
             "disabledButton",
           ]).instructions(),
         ),
-        ...Array.from({ length: 31 }, (_, i) => i + 1).map((day: number) =>
-          new button(day.toString(), () => {}, ["squareButton"]).instructions(),
+        ...Array.from({ length: this.getDaysInMonth() }, (_, i) => i + 1).map(
+          (day: number) =>
+            new button(day.toString(), () => {}, [
+              "squareButton",
+            ]).instructions(),
         ),
       ],
     };
+  }
+
+  private jsWeekdayToOffset(jsWeekDay: number): number {
+    // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+    // bloody americans, bloody javascript
+
+    if (jsWeekDay === 0) {
+      return 6;
+    }
+    return jsWeekDay - 1;
+  }
+
+  private getWeekdayOfFirstDay(): number {
+    return new Date(this.year, this.month, 1).getDay();
+  }
+
+  private getDaysInMonth(): number {
+    return new Date(this.year, this.month + 1, 0).getDate();
   }
 
   public unload() {}
--- src/timeShift/application/public/mainScreen/components/calendarPanel/calendarPanel.ts
+++ src/timeShift/application/public/mainScreen/components/calendarPanel/calendarPanel.ts
@@ -1,4 +1,11 @@
 class calendarPanel implements Component {
+  private readonly calendarId: string = "calendar" + Math.random().toString(36);
+  private readonly monthOutLabelId: string =
+    "monthOutLabel" + Math.random().toString(36);
+
+  private currentMonth: number = new Date().getMonth();
+  private currentYear: number = new Date().getFullYear();
+
   public render(parent: edomElement) {
     edom.fromTemplate([this.instructions()], parent);
   }
@@ -12,18 +19,77 @@ class calendarPanel implements Component {
           tag: "div",
           classes: ["calendarHeader"],
           children: [
-            new button("<", () => {}, ["squareButtonSmol"]).instructions(),
+            new button("<", this.decrementMonth.bind(this), [
+              "squareButtonSmol",
+            ]).instructions(),
             {
               tag: "label",
-              text: "Juli 2024",
+              text: `${this.getCurrentMonthName()} ${this.getCurrentYear()}`,
+              id: this.monthOutLabelId,
             },
-            new button(">", () => {}, ["squareButtonSmol"]).instructions(),
+            new button(">", this.incrementMonth.bind(this), [
+              "squareButtonSmol",
+            ]).instructions(),
           ],
         },
-        new calendar().instructions(),
+        this.createCalendar(),
       ],
     };
   }
 
   public unload() {}
+
+  private decrementMonth(self: edomElement) {
+    this.currentMonth--;
+
+    if (this.currentMonth < 0) {
+      this.currentMonth = 11;
+      this.currentYear--;
+    }
+
+    this.updateCalendarComponent(self);
+    this.updateMonthOutLabel();
+  }
+
+  private incrementMonth(self: edomElement) {
+    this.currentMonth++;
+
+    if (this.currentMonth > 11) {
+      this.currentMonth = 0;
+      this.currentYear++;
+    }
+
+    this.updateCalendarComponent(self);
+    this.updateMonthOutLabel();
+  }
+
+  private updateCalendarComponent(bttn: edomElement) {
+    const panel = bttn.parent?.parent;
+    edom.findById(this.calendarId)?.delete();
+    edom.fromTemplate([this.createCalendar()], panel);
+  }
+
+  private updateMonthOutLabel() {
+    edom.findById(this.monthOutLabelId)!.text =
+      `${this.getCurrentMonthName()} ${this.getCurrentYear()}`;
+  }
+
+  private createCalendar(): edomTemplate {
+    const _calendar: edomTemplate = new calendar(
+      this.currentMonth,
+      this.currentYear,
+    ).instructions();
+    _calendar.id = this.calendarId;
+    return _calendar;
+  }
+
+  private getCurrentMonthName(): string {
+    return new Date(this.currentYear, this.currentMonth).toLocaleString("de", {
+      month: "long",
+    });
+  }
+
+  private getCurrentYear(): number {
+    return this.currentYear;
+  }
 }
